c3ed50b97d7d7e549c0e8fc0c89d9320c6d1d199
ADITYA RAJ <adityaraj6220@gmail.com>
2024-06-06 21:59:18 +0530

    [1.8.5] Minor Cplaced template modify

--- templates/cplaced.js
+++ templates/cplaced.js
@@ -1,29 +1,20 @@
 const cplaced = (itemdetails, paymentdata) => {
-    // console.log(data);
-    // return
-    // Sample data
-    // data = [
-    //     { itemName: "PANNER BUTTER MASALA COMBO", quantity: 1, price: 200 }
-    //     // Add more items as needed
-    // ];
-
-    // Initialize the HTML string with the template
     var htmlString = `
 <tbody>
     <tr>
         <td style="width: 20px; text-align: center; height: 28px;border: 1px solid black;border-collapse: collapse;">
             <b>SL. No</b>.<br>
         </td>
-        <td style="width: 200px; text-align:center">
+        <td style="width: 200px;border: 1px solid black; text-align: center;">
             <div><b>ITEMS</b><br></div>
         </td>
-        <td style="width: 30px;text-align:center">
+        <td style="width: 30px;text-align:center;border: 1px solid black; text-align: center;">
             <div><b>QTY</b><br></div>
         </td>
-        <td style="width: 20px; text-align:center">
+        <td style="width: 20px; text-align:center;border: 1px solid black; text-align: center;">
             <div><b>UNIT PRICE</b><br></div>
         </td>
-        <td style="width: 50px; text-align:center">
+        <td style="width: 50px; text-align:center;border: 1px solid black; text-align: center;">
             <div><b>TOTAL</b><br></div>
         </td>
     </tr>
@@ -34,19 +25,19 @@ const cplaced = (itemdetails, paymentdata) => {
 
         htmlString += `
     <tr>
-        <td style="width: 20px; text-align: center; height: 28px;">
+        <td style="width: 20px; text-align: center; height: 28px;border: 1px solid black">
             <div>${i + 1}<br></div>
         </td>
-        <td style="width: 200px;padding-left:16px">
+        <td style="width: 200px;padding-left:16px;border: 1px solid black">
             <div>${itemdetails[i].itemname}<br></div>
         </td>
-        <td style="width: 30px; text-align:center">
+        <td style="width: 30px; text-align:center;border: 1px solid black">
             <div>${itemdetails[i].quantity}<br></div>
         </td>
-        <td style="width: 30px; text-align:center">
+        <td style="width: 30px; text-align:center;border: 1px solid black">
             <div>${itemdetails[i].price}<br></div>
         </td>
-        <td style="width: 30px; text-align:center">
+        <td style="width: 30px; text-align:center;border: 1px solid black">
             <div>${itemdetails[i].quantity * itemdetails[i].price}<br></div>
         </td>
     </tr>
@@ -55,15 +46,12 @@ const cplaced = (itemdetails, paymentdata) => {
 
     htmlString += `
 <tr>
-    <td>
-        <div><br></div>
-    </td>
-    <td colspan="3" style="width: 150px;text-align:right;padding-right: 16px">
+    <td colspan="4" style="width: 150px;text-align:right;padding-right: 16px;border: 1px solid black; padding-top: 8px;padding-bottom:8px">
         <div><b>DELIVERY CHARGE</b> <br></div>
     </td>
-    <td style="width: 50px; text-align:center">
+    <td style="width: 50px; text-align:center;border: 1px solid black; padding-top: 8px;padding-bottom:8px">
         <div>
-            <b><span class="size" style="font-size:12px;color:green"> FREE</span></b><br>
+            <b><span class="size" style="color:green"> FREE</span></b><br>
         </div>
     </td>
 </tr>`;
@@ -71,15 +59,12 @@ const cplaced = (itemdetails, paymentdata) => {
         if (paymentdata.hasOwnProperty(key)) {
             htmlString += `
             <tr>
-                <td>
-                    <div><br></div>
+                <td colspan="4" style="width: 150px;text-align:right;padding-right: 16px;border: 1px solid black; padding-top: 8px;padding-bottom:8px">
+                    <div style="text-transform:uppercase"><b>${key}</b> <br></div>
                 </td>
-                <td colspan="3" style="width: 150px;text-align:right;padding-right: 16px">
-                    <div style="text-transform:uppercase;font-size:12px"><b>${key}</b> <br></div>
-                </td>
-                <td style="width: 50px; text-align:center">
+                <td style="width: 50px; text-align:center;border: 1px solid black; padding-top: 8px;padding-bottom:8px">
                     <div>
-                        <b><span class="size" style="font-size:16px;font-size:12px"> ${paymentdata[key]}</span></b><br>
+                        <b><span class="size"> ${paymentdata[key]}</span></b><br>
                     </div>
                 </td>
             </tr>
